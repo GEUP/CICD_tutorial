3e491a6897abaa69f9df40ddeac96f7ca58427e6
unknown <knmj789@gmail.com>
2022-06-23 20:45:42 +0900

    fix test case

--- testcode.py
+++ testcode.py
@@ -6,18 +6,10 @@ class TestAdder(unittest.TestCase):
     def test_adder_case1(self):
         my_adder = Adder()
         self.assertEqual(my_adder.add(1,2), 3)
-
-    def test_adder_case2(self):
-        my_adder = Adder()
-        self.assertEqual(my_adder.add(2,2), 4)
-    
-    def test_adder_case3(self):
-        my_adder = Adder()
-        self.assertEqual(my_adder.add(4,4), 8)
     
     def test_adder_case_fail(self):
         my_adder = Adder()
-        self.assertEqual(my_adder.add(1,1), -1)
+        self.assertEqual(my_adder.add(1,1), 2)
   
 
 if __name__=='__main__':
